1e78601d18ed319b7c8ad33dba0381cee0952735
user <dvd.off@mail.ru>
2021-02-16 01:46:27 +0700

    add commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("tom");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("tom");
+
+        System.out.println(bean2.getMessage().equals(bean.getMessage()));
+        System.out.println(beanCat2.getName().equals(beanCat.getName()));
     }
 }
